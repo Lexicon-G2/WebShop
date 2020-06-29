007b3156a9b7abdf52f564bc0fe3f5ca22a0b216
sundqvist <43414280+sundqvist@users.noreply.github.com>
2020-06-29 13:25:53 +0200

    adding custom password setting

--- WebAppForWebshop/WebAppForWebshop/Startup.cs
+++ WebAppForWebshop/WebAppForWebshop/Startup.cs
@@ -66,7 +66,7 @@ namespace WebAppForWebshop
                 options.AccessDeniedPath = "/Identity/Account/AccessDenied";
                 options.SlidingExpiration = true;
             });
-        }
+        
     }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
